8cdb6d9b35354c672eea66ceae76bfd32a8d10bc
DamirSul <suleimenov.damirrr@mail.ru>
2023-10-16 22:50:13 +0600

    add changes at lab

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    //
+    public function __invoke(Request $request)
+    {
+        $login = $request->get('login');
+        if (!$login){
+            return "Login is required";
+        }
+        $password = $request->get('password');
+        if (!$password){
+            return "Password is required";
+        }
+        if(strlen($password) < 8){
+            return "Password is weak, minimum 8 chars";
+        }
+        if(User::where('login', $login)->exists()){//user already reg
+            return "Login is already exists";
+        }
+        return User::create([
+            'password' => Hash::make($password),
+            'login' => $login
+           // 'email' => 'test@mail.ru'
+        ]);
+    }
 }
